197029b87c4f14b32f62d09f4031ed3be47496d5
Guilherme Padilha <gs.padilha@gmail.com>
2023-06-09 15:19:36 -0300

    sort

---
+++ src/day1/Stack.ts
@@ -0,0 +1,47 @@
+type QNode<T> = {
+    value: T;
+    prev?: QNode<T>;
+};
+
+export default class Stack<T> {
+    public length: number;
+    private head?: QNode<T>;
+
+    constructor() {
+        this.head = undefined;
+        this.length = 0;
+    }
+
+    push(item: T): void {
+        const node = { value: item } as QNode<T>;
+
+        this.length++;
+
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
+
+    pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.value;
+        }
+
+        const head = this.head;
+        this.head = this.head?.prev;
+
+        return head?.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
+}
